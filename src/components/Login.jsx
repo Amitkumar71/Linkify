f2505eaef4b51622921b96e2c33829a78b088020
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-09-07 22:18:24 +0530

    Alert

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,44 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Login.scss';
+
+const AutoDismissAlert = ({ message, onClose }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 1500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [onClose]);
+
+  return (
+    <div className="auto-dismiss-alert">
+      <p>{message}</p>
+    </div>
+  );
+};
 
 const Login = ({ onLogin }) => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [profession, setProfession] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
 
   const handleStartClick = () => {
-    // Check if all fields are filled before proceeding
     if (name && email && profession) {
       onLogin();
     } else {
-      alert('Please fill in all fields.');
+      setShowAlert(true);
     }
+  };
+
+  const handleCloseAlert = () => {
+    setShowAlert(false);
   };
 
   return (
     <div className="login-container">
       <h2>Login</h2>
-     <div className='login-body'> 
-      <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Profession"
-        value={profession}
-        onChange={(e) => setProfession(e.target.value)}
-      />
-      <button className="start" onClick={handleStartClick}>Start</button>
-    </div>
+      <div className="login-body">
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Profession"
+          value={profession}
+          onChange={(e) => setProfession(e.target.value)}
+        />
+        <button className="start" onClick={handleStartClick}>
+          Start
+        </button>
+      </div>
+      {showAlert && (
+        <AutoDismissAlert message="Please fill in all fields." onClose={handleCloseAlert} />
+      )}
     </div>
   );
 };
